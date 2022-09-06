d39b386172678e17343676204400ba9747586c4e
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-09-06 08:13:10 +0300

    version

--- winnie32api/__init__.py
+++ winnie32api/__init__.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 
 __title__ = "winnie32api"
 __author__ = "Booplicate"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .mouse import *
 from .windows import *
